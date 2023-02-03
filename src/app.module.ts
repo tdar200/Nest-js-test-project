63fd168682604a1e89405fea0cefc67624a06fbb
Gigalabs <gigalabs@Gigalabss-MacBook-Pro.local>
2023-02-03 16:48:47 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksController } from './tasks/tasks.controller';
+@Module({
+  imports: [TasksModule],
+  controllers: [TasksController],
+})
+export class AppModule {}
